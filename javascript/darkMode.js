00266c07f7eb16afa64a6eecaab1797d81528f02
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-05-06 19:24:38 -0300

    modo escuro adicionado

---
+++ javascript/darkMode.js
@@ -0,0 +1,13 @@
+const darkMode = document.getElementById("btnDarkMode");
+
+darkMode.addEventListener("click", function(){
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        darkMode.innerText = "sunny";
+
+        
+    } else {
+        darkMode.innerText = "dark_mode";
+    }
+});
